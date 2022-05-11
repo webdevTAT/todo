f37c23f7fe48ac9dc44e19ab885126d399321806
Tanvir Tonoy <forlonguse.tat@outlook.com>
2022-05-11 21:10:32 +0600

    new update

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-
-// working the checkbox function
 //now we will work with the add button
 //itemContainer
-function addClicked(){
-    let container = document.getElementById("itemContainer");
-    let userInput = document.getElementById("text").value;
-    let Inputbox = document.getElementById("text");
-    let addButton = document.getElementsByClassName("add")[0];
-    if(userInput.length != 0){
-       let domElement = document.createElement("li");
-       let checkboxCreation = document.createElement("input");
-       checkboxCreation.setAttribute("type","checkbox");
-       let elementText = document.createTextNode(userInput);
-       domElement.appendChild(checkboxCreation);
-       domElement.appendChild(elementText);
-       container.appendChild(domElement);
-    }
-    else{
-       Inputbox.style.backgroundColor = "rgb(255, 241, 240)"
-    }
+function addClicked() {
+  let Inputbox = document.getElementById("text");
+  let container = document.getElementById("itemContainer");
+  let userInput = document.getElementById("text").value;
+  let addButton = document.getElementsByClassName("add")[0];
+  if (userInput.length != 0) {
+   // document.getElementsByClassName('cbx')[0].style.backgroundColor = "green";
+    let domElement = document.createElement("li");
+    let checkboxCreation = document.createElement("input");
+    checkboxCreation.setAttribute("type", "checkbox");
+    checkboxCreation.setAttribute("class", "cbx");
+    let elementText = document.createTextNode(userInput);
+    let textSpan = document.createElement("span");
+    textSpan.setAttribute("class", "spanText");
+    textSpan.appendChild(elementText);
+    domElement.appendChild(checkboxCreation);
+    //  domElement.appendChild(elementText);
+    domElement.appendChild(textSpan);
+    container.appendChild(domElement);
+    Inputbox.value = "";
+  } else {
+    Inputbox.style.backgroundColor = "rgb(255, 241, 240)";
+    Inputbox.setAttribute("placeholder", "লিখা কই?😑add new");
+  }
 }
+// now lets check to create
